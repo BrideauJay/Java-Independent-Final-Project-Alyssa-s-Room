15aef070a2ba64437081beef140d06d3a5a34c14
Jessica Brideau <jessicab987@comcast.net>
2016-04-25 18:58:45 -0400

    Cleaned comments, formatted

--- Item.java
+++ Item.java
@@ -4,119 +4,176 @@ import javafx.scene.paint.Color;
 
 public class Item {
 
-	//initialize data fields
+	
+	/**
+	 * initialize data fields
+	 */
 	private Furniture HeldBy;
-	
+
 	private String Information;
-	
+
 	private String Reaction;
-	
+
 	public Shape sprite;
-	
+
 	private Color color;
-	
-	
-//	currently scrapped code for simpler creation of items
-//  may be implemented in next stage
-	//simple constructor to be overloaded based on input
-//	public Item(Furniture furniture, String itemName, Color color){
-//		switch(itemName){
-//		case "brush": 
-//			new Item(furniture, "A hairbrush. There's still some hair caught in it.", "Weird. I didn't think Alyssa would have a hairbrush.", new Rectangle (0,0,15,5), color);
-//			break;
-//		case "pillow":
-//			new Item(furniture, "A regular pillow.", "Looks like it's been in a few fights. Pillow fights.", new Rectangle (0,0,40,40), color);
-//			break;
-//		}
-//	}
-	
-	//item constructor that takes furniture and other information
-	public Item(Furniture furniture, String information, String reaction, Shape sprite, Color color){
-		setHolding(furniture); //set the furniture holding the item
-		setInfo(information); //set the item's information
+
+
+	/**
+	 * item constructor that takes furniture and other information
+	 * 
+	 * @param furniture
+	 * @param information
+	 * @param reaction
+	 * @param sprite
+	 * @param color
+	 */
+	public Item(Furniture furniture, String information, String reaction, Shape sprite, Color color) {
+		setHolding(furniture); // set the furniture holding the item
+		setInfo(information); // set the item's information
 		setReact(reaction);
 		setSprite(sprite);
 		setColor(color);
-		}
-	
-	//method to set the furniture holding the item
-	private void setHolding(Furniture f){
+	}
+
+	/**
+	 * method to set the furniture holding the item
+	 * 
+	 * @param f
+	 */
+	private void setHolding(Furniture f) {
 		this.HeldBy = f;
 	}
-	
-	//method to get furniture item holding item
-	public Furniture getHeldBy(){
+
+	/**
+	 * method to get furniture item holding item
+	 * 
+	 * @return
+	 */
+	public Furniture getHeldBy() {
 		return this.HeldBy;
 	}
-	
-	//set the item's information
-	private void setInfo(String info){
+
+	/**
+	 * set the item's information
+	 * 
+	 * @param info
+	 */
+	private void setInfo(String info) {
 		this.Information = info;
 	}
-	
-	//get the item's information
-	public String getInfo(){
+
+	/**
+	 * get the item's information
+	 * 
+	 * @return
+	 */
+	public String getInfo() {
 		return this.Information;
 	}
-	
-	//set the reaction the player will have to the item
-	private void setReact(String react){
+
+	/**
+	 * set the reaction the player will have to the item
+	 * 
+	 * @param react
+	 */
+	private void setReact(String react) {
 		this.Reaction = react;
 	}
-	
-	//get the reaction the player has to the item
-	public String getReact(){
+
+	/**
+	 * get the reaction the player has to the item
+	 * 
+	 * @return
+	 */
+	public String getReact() {
 		return this.Reaction;
 	}
-	
-	//method to set the item's sprite
-	private void setSprite(Shape sprite){
+
+	/**
+	 * method to set the item's sprite
+	 * 
+	 * @param sprite
+	 */
+	private void setSprite(Shape sprite) {
 		this.sprite = sprite;
 	}
-	
-	//method to get the item's sprite
-	public Shape getSprite(){
+
+	/**
+	 * method to get the item's sprite
+	 * 
+	 * @return
+	 */
+	public Shape getSprite() {
 		return this.sprite;
 	}
-	
-	/**method to get x value of sprite */
-	public double getX(){
-		Rectangle sprite = (Rectangle)getSprite();
+
+	/**
+	 * method to get x value of item sprite
+	 * 
+	 * @return
+	 */
+	public double getX() {
+		Rectangle sprite = (Rectangle) getSprite();
 		return sprite.getX();
 	}
-	
-	//method to set the x value for the item sprite
-		public void setX(double x){
-			Rectangle sprite = (Rectangle)getSprite();
-			sprite.setX(x);
-		}
-		
-		//method to return current y value of player sprite
-		public double getY(){
-			Rectangle sprite = (Rectangle)getSprite();
-			return sprite.getY();
-		}
-		
-		//method to set the y value for the player's sprite
-		public void setY(double y){
-			Rectangle sprite = (Rectangle)getSprite();
-			sprite.setY(y);
-		}
-		
-		/**method to get a point2d of the x and y of the player */
-		public Point2D getCorner(){
-			Point2D corner = new Point2D(getX(), getY());
-			return corner;
-		}
-	
-	//method to set color
-	public void setColor(Color color){
+
+	/**
+	 * method to set the x value for the item sprite
+	 * 
+	 * @param x
+	 */
+	public void setX(double x) {
+		Rectangle sprite = (Rectangle) getSprite();
+		sprite.setX(x);
+	}
+
+	/**
+	 * method to return current y value of item sprite
+	 * 
+	 * @return
+	 */
+	public double getY() {
+		Rectangle sprite = (Rectangle) getSprite();
+		return sprite.getY();
+	}
+
+	/**
+	 * method to set the y value for the item sprite
+	 * 
+	 * @param y
+	 */
+	public void setY(double y) {
+		Rectangle sprite = (Rectangle) getSprite();
+		sprite.setY(y);
+	}
+
+	/**
+	 * method to get a Point2d of the x and y of the Item
+	 * 
+	 * @return
+	 */
+	public Point2D getCorner() {
+		Point2D corner = new Point2D(getX(), getY());
+		return corner;
+	}
+
+	/**
+	 * method to set color of item
+	 * 
+	 * @param color
+	 */
+	public void setColor(Color color) {
 		this.color = color;
 		this.sprite.setFill(color);
 	}
-	
-	//method to get color
-	public Color getColor(){
+
+	/**
+	 * method to get color of item
+	 * 
+	 * @return
+	 */
+	public Color getColor() {
 		return this.color;
 	}
 }
